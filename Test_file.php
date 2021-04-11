7a076f8714155248f074afe9c4fea2862a3c1f80
GoHe-Aurora <72535494+GoHe-Aurora@users.noreply.github.com>
2021-04-11 03:22:27 -0500

    12 Keeping the user logged in

--- Test_file.php
+++ Test_file.php
@@ -3,3 +3,4 @@ Video 8 08 Requesting the token in our app
 Video 09 Storing the authenticated user
 Video 10 Using a device id for the token name
 Video 11 Storing the token securely
+Video 12 12 Keeping the user logged in
