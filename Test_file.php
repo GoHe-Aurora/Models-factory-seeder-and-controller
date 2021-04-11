a1fd238da90843988cc5e9daa11901772333e484
GoHe-Aurora <72535494+GoHe-Aurora@users.noreply.github.com>
2021-04-11 04:06:06 -0500

    13 Creating a posts endpoint

--- Test_file.php
+++ Test_file.php
@@ -4,3 +4,4 @@ Video 09 Storing the authenticated user
 Video 10 Using a device id for the token name
 Video 11 Storing the token securely
 Video 12 12 Keeping the user logged in
+Video 13 Creating a posts endpoint
